9276f5817e18107f40f0a533df783708081e83e8
nopdan <me@nopdan.com>
2023-05-07 00:50:32 +0800

    new phrase.txt

--- README.md
+++ README.md
@@ -4,9 +4,10 @@
 和词语拼音数据 [mozillazg/phrase-pinyin-data](https://github.com/mozillazg/phrase-pinyin-data)  
 转换为更适合拼音输入法的格式
 
-|                 | 来源                                | 备注                     |
-| --------------- | ----------------------------------- | ------------------------ |
-| chars.txt       | pinyin-data/pinyin.txt              | 汉字拼音数据             |
-| words.txt       | phrase-pinyin-data/pinyin.txt       | 词语拼音数据             |
-| words_large.txt | phrase-pinyin-data/large_pinyin.txt | 词语拼音数据             |
-| correct.txt     |                                     | 补充或对以上数据进行修正 |
+|             | 来源                                | 备注                     |
+| ----------- | ----------------------------------- | ------------------------ |
+| pinyin.txt  | pinyin-data/pinyin.txt              | 汉字拼音数据             |
+| phrase.txt  | phrase-pinyin-data/pinyin.txt       | 词语拼音数据             |
+| large.txt   | phrase-pinyin-data/large_pinyin.txt | 词语拼音数据             |
+| correct.txt | 部分来自深蓝词库转换                | 补充或对以上数据进行修正 |
+| small.txt   |                                     | 筛选出带多音字的词       |
--- main.py
+++ main.py
@@ -21,6 +21,15 @@ def conv(pinyin):
     return res
 
 
+# 多音字，value 为最常用的读音
+duoyinzi = {}
+# 排除一些多音字，不同读音的频率相当
+exclude = set(["长", "重", "行", "还", "着", "调", "传", "乐", "藏", "朝", "觉", "奇", "恶", "地"])
+# 带有多音字的词
+words = []
+words_set = set()
+
+
 def convert(path, save_path, isWord=False):
     f = open(path, "r", encoding="utf-8")
     lines = f.readlines()
@@ -45,12 +54,16 @@ def convChar(line, fout):
     char = chr(int(char, 16))
     fout.write(char)
     fout.write("\t")
+
     # 转换拼音
     pys = res.group(2).split(",")
     pys = [conv(py) for py in pys]
     pys = list(OrderedDict.fromkeys(pys))  # 去重
     fout.write(" ".join(pys))
     fout.write("\n")
+    # 多音字
+    if len(pys) != 1:
+        duoyinzi[char] = pys[0]
 
 
 # 转换词组拼音数据
@@ -69,9 +82,34 @@ def convWord(line, fout):
     fout.write(" ".join(pys))
     fout.write("\n")
 
+    if len(word) != len(pys):
+        return
+
+    # 含有多音字且不是最常用的读音
+    flag = False
+    for i, char in enumerate(word):
+        if char in exclude:
+            flag = True
+            break
+        if char in duoyinzi and duoyinzi[char] != pys[i]:
+            flag = True
+            break
+    if flag:
+        entry = word + "\t" + " ".join(pys)
+        if entry not in words_set:
+            words_set.add(entry)
+            words.append(entry)
+
 
 if __name__ == "__main__":
-    convert("./pinyin-data/pinyin.txt", "chars.txt")
+    convert("./pinyin-data/pinyin.txt", "pinyin.txt")
 
-    convert("./phrase-pinyin-data/pinyin.txt", "words.txt", isWord=True)
-    convert("./phrase-pinyin-data/large_pinyin.txt", "words_large.txt", isWord=True)
+    convert("./phrase-pinyin-data/pinyin.txt", "phrase.txt", isWord=True)
+    convert("./phrase-pinyin-data/large_pinyin.txt", "large.txt", isWord=True)
+
+    fout = open("small.txt", "wt", encoding="utf-8")
+    # words = list(OrderedDict.fromkeys(words))
+    for entry in words:
+        fout.write(entry)
+        fout.write("\n")
+    fout.close()
